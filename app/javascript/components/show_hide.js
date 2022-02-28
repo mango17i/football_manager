095484e6af42328676094e4d91fed3d0e8d68b8d
issam hadjal <issam.hadjal@gmail.com>
2022-02-28 20:59:16 +0100

    end toggle

--- app/javascript/components/show_hide.js
+++ app/javascript/components/show_hide.js
@@ -1,1 +1,12 @@
+const showHide = () => {
+    let cards = document.querySelectorAll('.card-category');
+    for (let i = 0; i < cards.length; i++) {
+      let card = cards[i];
+      let cardToggle = function () {
+        this.classList.toggle('active');
+      };
+      card.addEventListener('click', cardToggle);
+    };
+};
+export {showHide};
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import { showHide } from "../components/show_hide";
+
+document.addEventListener('turbolinks:load', () => {
+    showHide();
+});
